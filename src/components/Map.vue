8c54b57bac64c77fe5b6ca79260f8f77e4e4ae31
ultraman-agul <agulagul@163.com>
2023-04-03 23:29:37 +0800

    feat: 绘制点线

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -20,8 +20,8 @@ const props = defineProps({
     apiConfig: {
         type: Object,
         default: () => ({ // 对象或数组默认值必须从一个工厂函数获取
-            // version: '2.0',
-            version: '1.4.15',
+            version: '2.0',
+            // version: '1.4.15',
             key: '67bc06bfdc3355fb7a17b5fea35de9cf',
             plugins: ["AMap.DistrictSearch", "AMap.ControlBar"],
         })
@@ -43,10 +43,10 @@ const props = defineProps({
     // 搜索范围[对应文档https://lbs.amap.com/api/javascript-api/reference/search#m_AMap.DistrictSearch]
     level: { type: String, default: "city" },
     // 是否展示卫星地图
-    isShowSatellite: { type: Boolean, default: true },
+    isShowSatellite: { type: Boolean, default: false },
     // 是否展示卫星路网
-    isShowRoadNet: { type: Boolean, default: true },
-    polylineConfig: {
+    isShowRoadNet: { type: Boolean, default: false },
+    polylineConfig: { // 边界线样式
         type: Object,
         default: () => ({
             // 是否显示边界线
@@ -97,8 +97,142 @@ const props = defineProps({
             backOrFront: "both",
         }),
     },
+     // 是否绘制线条
+    isDrawPolyLine: { type: Boolean, default: true },
+    // 线条数据
+    polyLineData: {
+        type: Array,
+        default: () => [
+            {
+                path: [
+                    [112.72571, 24.449534],
+                    [113.245419, 24.168629],
+                    [113.194628, 23.791832]
+                ],
+                // 是否显示描边
+                isOutline: true,
+                // 线条描边颜色
+                outlineColor: "#ffeeff",
+                // 描边的宽度
+                borderWeight: 3,
+                // 线条颜色
+                strokeColor: "#3366FF",
+                // 线条透明度，取值范围[0,1]，默认0.9
+                strokeOpacity: 1,
+                // 线条宽度
+                strokeWeight: 6,
+                // 线条样式，实线:solid，虚线:dashed
+                strokeStyle: "solid",
+                // 勾勒形状轮廓的虚线和间隙的样式
+                strokeDasharray: [10, 5],
+                // 折线拐点的绘制样式，默认值为'miter'尖角，其他可选值：'round'圆角、'bevel'斜角
+                lineJoin: "round",
+                // 折线两端线帽的绘制样式，默认值为'butt'无头，其他可选值：'round'圆头、'square'方头
+                lineCap: "round",
+                // 折线覆盖物的叠加顺序
+                zIndex: 50,
+            },
+            {
+                path: [
+                    [112.623349, 24.045431],
+                    [112.793974, 23.74417]
+                ],
+                // 是否显示描边
+                isOutline: true,
+                // 线条描边颜色
+                outlineColor: "#ffeeff",
+                // 描边的宽度
+                borderWeight: 3,
+                // 线条颜色
+                strokeColor: "#3366FF",
+                // 线条透明度，取值范围[0,1]，默认0.9
+                strokeOpacity: 1,
+                // 线条宽度
+                strokeWeight: 6,
+                // 线条样式，实线:solid，虚线:dashed
+                strokeStyle: "solid",
+                // 勾勒形状轮廓的虚线和间隙的样式
+                strokeDasharray: [10, 5],
+                // 折线拐点的绘制样式，默认值为'miter'尖角，其他可选值：'round'圆角、'bevel'斜角
+                lineJoin: "round",
+                // 折线两端线帽的绘制样式，默认值为'butt'无头，其他可选值：'round'圆头、'square'方头
+                lineCap: "round",
+                // 折线覆盖物的叠加顺序
+                zIndex: 50,
+            },
+        ],
+    },
+     // 是否绘制点位
+    isDrawPoint: { type: Boolean, default: true },
+    // 点位数据
+    pointData: {
+        type: Array,
+        default: () => [
+            {
+                // 唯一值
+                iden: "点位1",
+                // 坐标
+                lngLat: [112.952847, 23.98007],
+                // marker点位基于坐标的偏移量
+                offset: [-13, -30],
+                // 自定义图标(Object可设置精灵图定位，String为图标地址)
+                icon: {
+                    // 图标大小
+                    size: [25, 34],
+                    // 图标地址
+                    image:
+                        "//a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png",
+                    // 图标所用的图片大小
+                    imageSize: [135, 40],
+                    // 图标取图偏移量(背景图定位)
+                    imageOffset: [-9, -3],
+                },
+            },
+            {
+                // 唯一值
+                iden: "点位2",
+                // 坐标
+                lngLat: [113.005128, 23.644603],
+                // marker点位基于坐标的偏移量
+                offset: [-13, -30],
+                // 自定义图标(Object可设置精灵图定位，String为图标地址)
+                // icon: "//a.amap.com/jsapi_demos/static/demo-center/icons/dir-via-marker.png",
+                 icon: {
+                    // 图标大小
+                    size: [25, 34],
+                    // 图标地址
+                    image:
+                        "//a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png",
+                    // 图标所用的图片大小
+                    imageSize: [135, 40],
+                    // 图标取图偏移量(背景图定位)
+                    imageOffset: [-9, -3],
+                },
+            },
+            {
+                // 唯一值
+                iden: "点位3",
+                // 坐标
+                lngLat: [113.425473, 24.106349],
+                // marker点位基于坐标的偏移量
+                offset: [-13, -30],
+                // 自定义图标(Object可设置精灵图定位，String为图标地址)
+                icon: {
+                    // 图标大小
+                    size: [25, 34],
+                    // 图标地址
+                    image:
+                        "//a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png",
+                    // 图标所用的图片大小
+                    imageSize: [135, 40],
+                    // 图标取图偏移量(背景图定位)
+                    imageOffset: [-95, -3],
+                },
+            },
+        ],
+    },
 })
-const mapInstance = ref(null) // 地图实例
+const mapInstance = ref<any>(null) // 地图实例
 let myAMap: any //
 
 // 渲染地图边界线
@@ -167,6 +301,86 @@ const renderControlBar = () => {
     }
 };
 
+// 渲染线条
+const renderLine = () => {
+     const { isDrawPolyLine, polyLineData }: any = props;
+    if (isDrawPolyLine) {
+        const polyLines = [];
+        for (let i = 0; i < polyLineData.length; i++) {
+            const polyline = new myAMap.Polyline({
+                // 线条坐标
+                path: polyLineData[i].path,
+                // 是否显示外线
+                isOutline: polyLineData[i].isOutline,
+                // 外线颜色
+                outlineColor: polyLineData[i].outlineColor,
+                // 外线宽度
+                borderWeight: polyLineData[i].borderWeight,
+                // 线条颜色
+                strokeColor: polyLineData[i].strokeColor,
+                // 线条透明度
+                strokeOpacity: polyLineData[i].strokeOpacity,
+                // 线条宽度
+                strokeWeight: polyLineData[i].strokeWeight,
+                // 线条样式，实线:solid，虚线:dashed
+                strokeStyle: polyLineData[i].strokeStyle,
+                // 勾勒形状轮廓的虚线和间隙的样式
+                strokeDasharray: polyLineData[i].strokeDasharray,
+                // 折线拐点的绘制样式
+                lineJoin: polyLineData[i].lineJoin,
+                // 折线两端线帽的绘制样式
+                lineCap: polyLineData[i].lineCap,
+                //  折线覆盖物的叠加顺序
+                zIndex: polyLineData[i].zIndex,
+            });
+            polyLines.push(polyline);
+        }
+        mapInstance.value.add(polyLines);
+    }
+};
+
+
+// 渲染点位
+const renderPoint = () => {
+    const { isDrawPoint, pointData }: any = props;
+
+    if (isDrawPoint) {
+        // styles存储所有的图标样式
+        const styles: any[] = [];
+        // 为了兼容原来的方式，构造一个新对象存储在styles
+        // anchor为图标在地图上的偏移量，url等同于Marker对象的Icon的image
+        pointData.forEach((point: any) => {
+            return !(styles.find(icon => icon.image == point.icon.image))
+                && styles.push({ 
+                    ...point.icon, 
+                    anchor: new myAMap.Pixel(15, 35), 
+                    url: point.icon.image
+                })
+        });
+
+        // 构造新的点位集合，因为MassMarks对象的点位中必须有lnglat经纬度
+        // 与Marker对象的经纬度区别在于Lat的L要小写
+        // style为当前点位的图标样式在styles中的索引
+        const points = pointData.map((point: any) => ({ ...point, lnglat: point.lngLat, style: styles.findIndex(icon => icon.url === point.icon.image) }));
+        console.log(points, styles);
+        
+        // 生成一个海量点图层，第一个参数为点位数据，第二个参数为一个options，选项样式
+        const massMarks = new myAMap.MassMarks(points, {
+            zIndex: 114,
+            zooms: [3, 19],
+            style: styles
+        });
+
+        // 将当前海量点图层添加到地图上
+        massMarks.setMap(mapInstance.value);
+
+        // 点位添加点击事件
+        massMarks.on("click", function ({ data }) {
+            console.log(data);
+        });
+    }
+};
+
 // 初始化地图
 const initMapInstance = (AMap: any) => {
     myAMap = AMap;
@@ -203,13 +417,15 @@ const initMapInstance = (AMap: any) => {
         console.log(option);
         
         mapInstance.value = new AMap.Map('map', option);
-        mapInstance.value.on('click', (e) => {
-            console.log(e);
+        mapInstance.value.on('click', (e: any) => {
+            console.log([e.lnglat.lng, e.lnglat.lat]);
         })
         // 渲染边界
         renderPolyLine(bounds);
         renderControlBar() // 渲染控制器
         render3dWall(bounds) // 渲染墙体
+        renderLine() // 渲染线条
+        renderPoint() // 渲染点位
     })
 
 }
