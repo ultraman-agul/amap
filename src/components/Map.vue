4e2be6fc050fbed35087529066932e6492d424c7
ultraman-agul <agulagul@163.com>
2023-04-09 17:51:34 +0800

    feat: massMarks绘制批量点

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,9 +1,3 @@
-<!--
- * new page
- * @author: zhaoyahui
- * @since: 2023-03-28
- * Map.vue
--->
 <template>
     <div id="map"></div>
 </template>
@@ -13,8 +7,8 @@ import AMapLoader from '@amap/amap-jsapi-loader'
 
 
 const props = defineProps({
-    mapStyle: { 
-        type: String, 
+    mapStyle: {
+        type: String,
         default: 'amap://styles/darkblue', // 默认暗黑系
     },
     apiConfig: {
@@ -34,7 +28,7 @@ const props = defineProps({
     zoom: { type: Number, default: 10 },
     // 地图显示的缩放级别范围
     zooms: { type: Array, default: () => [3, 18] },
-     // 区域名
+    // 区域名
     areaName: { type: String, default: "清远市" },
     // 显示下级行政区级数,行政区级别包括：国家、省/直辖市、市、区/县4个级别
     subdistrict: { type: Number, default: 0 },
@@ -59,7 +53,7 @@ const props = defineProps({
             strokeWeight: 4,
         }),
     },
-     // 是否3D显示
+    // 是否3D显示
     isShow3D: { type: Boolean, default: true },
     // 俯视角度
     pitch: { type: Number, default: 40 },
@@ -97,7 +91,7 @@ const props = defineProps({
             backOrFront: "both",
         }),
     },
-     // 是否绘制线条
+    // 是否绘制线条
     isDrawPolyLine: { type: Boolean, default: true },
     // 线条数据
     polyLineData: {
@@ -162,7 +156,7 @@ const props = defineProps({
             },
         ],
     },
-     // 是否绘制点位
+    // 是否绘制点位
     isDrawPoint: { type: Boolean, default: true },
     // 点位数据
     pointData: {
@@ -174,18 +168,17 @@ const props = defineProps({
                 // 坐标
                 lngLat: [112.952847, 23.98007],
                 // marker点位基于坐标的偏移量
-                offset: [-13, -30],
+                // offset: [-13, -30],
                 // 自定义图标(Object可设置精灵图定位，String为图标地址)
                 icon: {
                     // 图标大小
                     size: [25, 34],
                     // 图标地址
-                    image:
-                        "//a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png",
+                    image: "//a.amap.com/lbs-dev-yuntu/static/web/image/common/newtag.png",
                     // 图标所用的图片大小
                     imageSize: [135, 40],
                     // 图标取图偏移量(背景图定位)
-                    imageOffset: [-9, -3],
+                    // imageOffset: [-9, -3],
                 },
             },
             {
@@ -194,19 +187,18 @@ const props = defineProps({
                 // 坐标
                 lngLat: [113.005128, 23.644603],
                 // marker点位基于坐标的偏移量
-                offset: [-13, -30],
+                // offset: [-13, -30],
                 // 自定义图标(Object可设置精灵图定位，String为图标地址)
                 // icon: "//a.amap.com/jsapi_demos/static/demo-center/icons/dir-via-marker.png",
-                 icon: {
+                icon: {
                     // 图标大小
                     size: [25, 34],
                     // 图标地址
-                    image:
-                        "//a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png",
+                    image: "//a.amap.com/jsapi_demos/static/demo-center/icons/dir-via-marker.png",
                     // 图标所用的图片大小
                     imageSize: [135, 40],
                     // 图标取图偏移量(背景图定位)
-                    imageOffset: [-9, -3],
+                    // imageOffset: [-9, -3],
                 },
             },
             {
@@ -215,18 +207,17 @@ const props = defineProps({
                 // 坐标
                 lngLat: [113.425473, 24.106349],
                 // marker点位基于坐标的偏移量
-                offset: [-13, -30],
+                // offset: [-13, -30],
                 // 自定义图标(Object可设置精灵图定位，String为图标地址)
                 icon: {
                     // 图标大小
                     size: [25, 34],
                     // 图标地址
-                    image:
-                        "//a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png",
+                    image: "//a.amap.com/lbs-dev-yuntu/static/web/image/common/newtag.png",
                     // 图标所用的图片大小
                     imageSize: [135, 40],
                     // 图标取图偏移量(背景图定位)
-                    imageOffset: [-95, -3],
+                    // imageOffset: [-95, -3],
                 },
             },
         ],
@@ -238,38 +229,38 @@ let myAMap: any //
 // 渲染地图边界线
 const renderPolyLine = (bounds = []) => {
     const { polylineConfig } = props
-    if(!polylineConfig.show){
-        return ;
+    if (!polylineConfig.show) {
+        return
     }
-     for (let i = 0; i < bounds.length; i++) {
+    for (let i = 0; i < bounds.length; i++) {
         new myAMap.Polyline({
             path: bounds[i],
             strokeColor: polylineConfig.strokeColor,
             strokeWeight: polylineConfig.strokeWeight,
             map: mapInstance.value,
-        });
+        })
     }
-} 
+}
 // 定义一个渲染3d墙体的方法
 const render3dWall = (bounds = []) => {
-    const { object3dWallConfig, apiConfig } = props;
+    const { object3dWallConfig, apiConfig } = props
     // 1.4.15版本的api通过Object3DLayer创建墙体
     if (apiConfig.version == "1.4.15") {
         if (object3dWallConfig.show) {
             // 定义一个3D图层
             const object3Dlayer = new myAMap.Object3DLayer({
                 zIndex: object3dWallConfig.zIndex,
-            });
+            })
             // 创建墙体
             const wall = new myAMap.Object3D.Wall({
                 path: bounds,
                 height: object3dWallConfig.wallHeight,
                 color: object3dWallConfig.color,
-            });
-            wall.transparent = object3dWallConfig.transparent;
-            wall.backOrFront = object3dWallConfig.backOrFront;
-            object3Dlayer.add(wall);
-            mapInstance.value.add(object3Dlayer);
+            })
+            wall.transparent = object3dWallConfig.transparent
+            wall.backOrFront = object3dWallConfig.backOrFront
+            object3Dlayer.add(wall)
+            mapInstance.value.add(object3Dlayer)
         }
     } else if (apiConfig.version == "2.0") {
         // 2.0版本的api通过描边添加墙体
@@ -279,14 +270,14 @@ const render3dWall = (bounds = []) => {
                 strokeColor: object3dWallConfig.color,
                 strokeWeight: object3dWallConfig.wallHeight,
                 map: mapInstance.value,
-            });
+            })
         }
     }
-};
+}
 
 // 定义一个渲染地图方位控制器
 const renderControlBar = () => {
-    const { controllBarConfig } = props;
+    const { controllBarConfig } = props
     if (controllBarConfig.show) {
         mapInstance.value.addControl(
             new myAMap.ControlBar({
@@ -297,15 +288,15 @@ const renderControlBar = () => {
                     top: `${controllBarConfig.positionTop}px`,
                 },
             }),
-        );
+        )
     }
-};
+}
 
 // 渲染线条
 const renderLine = () => {
-     const { isDrawPolyLine, polyLineData }: any = props;
+    const { isDrawPolyLine, polyLineData }: any = props
     if (isDrawPolyLine) {
-        const polyLines = [];
+        const polyLines = []
         for (let i = 0; i < polyLineData.length; i++) {
             const polyline = new myAMap.Polyline({
                 // 线条坐标
@@ -332,76 +323,73 @@ const renderLine = () => {
                 lineCap: polyLineData[i].lineCap,
                 //  折线覆盖物的叠加顺序
                 zIndex: polyLineData[i].zIndex,
-            });
-            polyLines.push(polyline);
+            })
+            polyLines.push(polyline)
         }
-        mapInstance.value.add(polyLines);
+        mapInstance.value.add(polyLines)
     }
-};
+}
 
 
 // 渲染点位
 const renderPoint = () => {
-    const { isDrawPoint, pointData }: any = props;
+    const { isDrawPoint, pointData }: any = props
 
     if (isDrawPoint) {
+        // 渲染批量点位使用MassMarks对象，相当于添加新的图层 - START
         // styles存储所有的图标样式
-        const styles: any[] = [];
+        const styles: any[] = []
         // 为了兼容原来的方式，构造一个新对象存储在styles
         // anchor为图标在地图上的偏移量，url等同于Marker对象的Icon的image
         pointData.forEach((point: any) => {
-            return !(styles.find(icon => icon.image == point.icon.image))
-                && styles.push({ 
-                    ...point.icon, 
-                    anchor: new myAMap.Pixel(15, 35), 
-                    url: point.icon.image
-                })
-        });
-
+            const findItem = styles.find(icon => icon.image == point.icon.image)
+            if (!findItem) {
+                styles.push({ ...point.icon, anchor: new myAMap.Pixel(15, 35), url: point.icon.image })
+            }
+        })
         // 构造新的点位集合，因为MassMarks对象的点位中必须有lnglat经纬度
         // 与Marker对象的经纬度区别在于Lat的L要小写
         // style为当前点位的图标样式在styles中的索引
-        const points = pointData.map((point: any) => ({ ...point, lnglat: point.lngLat, style: styles.findIndex(icon => icon.url === point.icon.image) }));
-        console.log(points, styles);
-        
+        const points = pointData.map((point: any) => ({ ...point, lnglat: point.lngLat, style: styles.findIndex(icon => icon.url === point.icon.image) }))
         // 生成一个海量点图层，第一个参数为点位数据，第二个参数为一个options，选项样式
         const massMarks = new myAMap.MassMarks(points, {
             zIndex: 114,
             zooms: [3, 19],
             style: styles
-        });
-
+        })
         // 将当前海量点图层添加到地图上
-        massMarks.setMap(mapInstance.value);
-
+        massMarks.setMap(mapInstance.value)
         // 点位添加点击事件
         massMarks.on("click", function ({ data }) {
-            console.log(data);
-        });
+            console.log(data)
+        })
+        console.log(massMarks)
+        massMarks.show = false
+        // 渲染批量点位使用MassMarks对象，相当于添加新的图层 - END
     }
-};
+}
 
 // 初始化地图
 const initMapInstance = (AMap: any) => {
-    myAMap = AMap;
+    myAMap = AMap
 
-    const option: {[key: string]: any} = {
+    const option: { [key: string]: any } = {
         mapStyle: props.mapStyle,
         center: props.center,
         zoom: props.zoom,
         zooms: props.zooms,
         doubleClickZoom: props.doubleClickZoom,
-         // 图层，卫星地图，卫星路网都属于图层，push到这个layers就可以了
+        // 图层，卫星地图，卫星路网都属于图层，push到这个layers就可以了
         layers: [],
         pitch: props.pitch,
     }
-    if(props.isShow3D){
+    if (props.isShow3D) {
         option.viewMode = '3D'
     }
-    if(props.isShowSatellite){ // 展示卫星图层
+    if (props.isShowSatellite) { // 展示卫星图层
         option.layers.push(new AMap.TileLayer.Satellite())
     }
-    if(props.isShowRoadNet){ // 展示路网图层
+    if (props.isShowRoadNet) { // 展示路网图层
         option.layers.push(new AMap.TileLayer.RoadNet())
     }
 
@@ -413,15 +401,15 @@ const initMapInstance = (AMap: any) => {
     console.log(district.search)
     district.search(props.areaName, function (status: string, res: any) {
         const bounds = res.districtList[0]['boundaries'] // 区域边界坐标
-        option.mask = bounds.map((i: any) => [i]); // 区域以外添加遮罩
-        console.log(option);
-        
-        mapInstance.value = new AMap.Map('map', option);
+        option.mask = bounds.map((i: any) => [i]) // 区域以外添加遮罩
+        console.log(option)
+
+        mapInstance.value = new AMap.Map('map', option)
         mapInstance.value.on('click', (e: any) => {
-            console.log([e.lnglat.lng, e.lnglat.lat]);
+            console.log([e.lnglat.lng, e.lnglat.lat])
         })
         // 渲染边界
-        renderPolyLine(bounds);
+        renderPolyLine(bounds)
         renderControlBar() // 渲染控制器
         render3dWall(bounds) // 渲染墙体
         renderLine() // 渲染线条
@@ -436,8 +424,8 @@ const loadAMap = () => {
         plugins: props.apiConfig.plugins,// 使用到的插件
     }).then((AMap) => {
         initMapInstance(AMap)
-    });
-};
+    })
+}
 
 onMounted(() => {
     loadAMap() // 加载地图
@@ -445,7 +433,7 @@ onMounted(() => {
 </script>
 
 <style scoped>
-#map{
+#map {
     height: 100vh;
     width: 100vw;
 }
